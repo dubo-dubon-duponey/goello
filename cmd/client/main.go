fa1b8a050bb3cc6e3579905bc870d2c3bd730dfb
dubo-dubon-duponey <dubodubonduponey+github@pm.me>
2021-02-23 13:05:58 -0800

    Fix output
    Signed-off-by: dubo-dubon-duponey <dubodubonduponey+github@pm.me>

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -21,25 +21,31 @@ var modeFlag = flag.String("m", "brute", "Resolution mode (brute or vanilla)")
 func main() {
 	flag.Parse()
 
-	if (*modeFlag == "vanilla") {
-    dom := fmt.Sprintf("%s.%s", strings.Trim(*instanceFlag, "."), strings.Trim(*domainFlag, "."))
-    ips, err := net.LookupIP(dom)
-    if err != nil {
-      fmt.Printf("FATAL: %s: %s", dom, err)
-      os.Exit(1)
-    }
-    jj, _ := json.Marshal(ips)
-    fmt.Printf(string(jj))
-    return
-  }
+	if *modeFlag == "vanilla" {
+		dom := fmt.Sprintf("%s.%s", strings.Trim(*instanceFlag, "."), strings.Trim(*domainFlag, "."))
+		ips, err := net.LookupIP(dom)
+		if err != nil {
+			fmt.Printf("FATAL: %s: %s", dom, err)
+			os.Exit(1)
+		}
+		jj, _ := json.Marshal(ips)
+		fmt.Printf(string(jj))
+		return
+	}
 
 	service := fmt.Sprintf("%s.%s.", strings.Trim(*serviceFlag, "."), strings.Trim(*domainFlag, "."))
-	instance := fmt.Sprintf("%s.%s.%s.", strings.Trim(*instanceFlag, "."), strings.Trim(*serviceFlag, "."), strings.Trim(*domainFlag, "."))
+	// instance := fmt.Sprintf("%s.%s.%s.", strings.Trim(*instanceFlag, "."), strings.Trim(*serviceFlag, "."), strings.Trim(*domainFlag, "."))
 
 	addFn := func(srv dnssd.Service) {
-		if srv.ServiceInstanceName() == instance {
+		j, _ := json.Marshal(srv)
+
+		fmt.Println(string(j))
+		//		fmt.Println(srv.ServiceInstanceName())
+		if srv.Host == strings.Trim(*instanceFlag, ".") {
+			//    if srv.ServiceInstanceName() == instance {
 			j, _ := json.Marshal(srv)
 
+			fmt.Println("FOUND ------->")
 			fmt.Println(string(j))
 
 			time.Sleep(1)
